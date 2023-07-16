be44f031fcf97578737bb35087e148c128ef5c4e
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2023-07-16 18:03:59 +0300

    update

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,6 +7,7 @@ Route::namespace('WebesemblyEditor\Controllers')->group(function() {
     Route::get('/editor-ui', 'WebesemblyEditorController@ui');
 
     Route::post('/webesembly/save-section', 'WebesemblyEditorController@saveSection');
+    Route::post('/webesembly/reset-section', 'WebesemblyEditorController@resetSection');
 
     Route::get('/webesembly-editor/webesembly-editor.js', 'WebesemblyAssetsController@editor');
     Route::get('/webesembly-editor/webesembly-iframe.css', 'WebesemblyAssetsController@iframeCss');
